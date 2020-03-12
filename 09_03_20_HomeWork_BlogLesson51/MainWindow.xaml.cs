34d67ce5b38e4e1bd6e10d0aa70f13cefa09ff93
Gil Muzika <gil_0_mu@hotmail.com>
2020-03-12 14:10:48 +0200

    Add project files.

---
+++ 09_03_20_HomeWork_BlogLesson51/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _09_03_20_HomeWork_BlogLesson51
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel _viewModel = new ViewModel();
+
+        public MainWindow()
+        {            
+            InitializeComponent();
+            DataContext = _viewModel;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.RuffleColors();
+            
+
+            await Task.Run(() => 
+            {
+                Timer locTimer = new Timer();
+                locTimer.Interval = 500;
+                locTimer.Elapsed += (object sender, ElapsedEventArgs e) => 
+                {
+                    SafeInvoke(() => { brdBorder.Background = _viewModel.BrushForBorder; });
+                };
+                locTimer.Start();
+                
+                
+            });
+        }
+
+        private void SafeInvoke(Action work)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                work.Invoke();
+                return;
+            }
+            this.Dispatcher.BeginInvoke(work);
+        }
+    }
+}
---
+++ 09_03_20_HomeWork_BlogLesson51/ViewModel/ViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Media;
+
+namespace _09_03_20_HomeWork_BlogLesson51
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Properties
+        private Brush _brushForBorder = Brushes.Gray;
+        public Brush BrushForBorder
+        {
+            get => _brushForBorder;
+            set
+            {
+                if (_brushForBorder == value) return;
+                _brushForBorder = value;
+                OnPropertyChanged("BrushForBorder");
+            }
+        }
+        private bool _changingBrushFlag = false;
+        public bool ChangingBrushFlag
+        {
+            get => _changingBrushFlag;
+            set
+            {
+                if (_changingBrushFlag == value) return;
+                _changingBrushFlag = value;
+                OnPropertyChanged("ChangingBrushFlag");
+            }
+        }
+        #endregion Properties
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        private void ChangeChangingBrushFlag()
+        {
+            ChangingBrushFlag = ChangingBrushFlag ? false : true;
+        }
+        private List<Brush> GetBrushes()
+        {
+            BrushConverter locBrushConverter = new BrushConverter();
+
+            List<Brush> brushes = new List<Brush>();
+            foreach(var s in typeof(Brushes).GetProperties())
+            {
+
+                if(s.PropertyType.Name == "SolidColorBrush")
+                {
+                    brushes.Add((Brush)locBrushConverter.ConvertFromString(s.Name));
+                }
+            }
+            return brushes;
+        }
+        public async Task<Brush> RuffleColors()
+        {                        
+            return await Task.Run(() =>
+            {     
+                
+
+                Random locRnd = new Random();
+                List<Brush> locBrushes = GetBrushes();
+                int count = 0;
+                Timer locTimer = new Timer();
+                locTimer.Interval = 10;
+                locTimer.Elapsed += (object sender, ElapsedEventArgs e) => 
+                    {
+                        
+                        if (count == 10) 
+                        {
+                            BrushForBorder = locBrushes[locRnd.Next(0, locBrushes.Count - 1)];                            
+                            ChangeChangingBrushFlag();
+                            count = 0;
+                        }
+                        if (count == 5) ChangeChangingBrushFlag();
+
+                        count++;                        
+                    };
+                locTimer.Start();
+                return BrushForBorder;
+            });
+        }
+
+
+
+    }
+}
